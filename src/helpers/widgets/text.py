5abba1fb6e2a5d4c35f030e4f223b701a031cab7
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-09 21:09:33 +0100

    fix

--- src/helpers/widgets/text.py
+++ src/helpers/widgets/text.py
@@ -1,6 +1,6 @@
 from helpers.widgets.widget import *
 from helpers.colors import Color, Colors
-from PIL.ImageFont import truetype as font
+from PIL.ImageFont import truetype as create_font
 from helpers.fonts import Font, Fonts
 
 
@@ -10,10 +10,10 @@ class Text(Widget):
         self.fontSize = fontSize
         self.color = color
         self.fontType = font.file
-        self.size = Size(*font(self.fontType, int(self.fontSize)).getsize(self.text))
+        self.size = Size(*create_font(self.fontType, int(self.fontSize)).getsize(self.text))
         print(self.size)
         super().__init__()
 
     def draw(self, canvas: ImageDraw, offset: Offset, max_size: Size, ratio: float) -> None:
-        self.font = font(self.fontType, int(self.fontSize * ratio))
+        self.font = create_font(self.fontType, int(self.fontSize * ratio))
         canvas.text((offset.dx*ratio, offset.dy*ratio), self.text, self.color.color, self.font)
