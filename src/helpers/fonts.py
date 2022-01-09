1d6a3e0c18e7803a4184d1de4ec0ee47ae66c304
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-09 21:23:32 +0100

    fix

--- src/helpers/fonts.py
+++ src/helpers/fonts.py
@@ -9,6 +9,7 @@ print(dirname(__file__))
 class Font:
     def __init__(self, file: str) -> None:
         self.file = file
+        print(self.file)
 
     def pil(self, fontsize: float) -> FreeTypeFont:
         return truetype(self.file, int(fontsize))
