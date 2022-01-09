b73894fc327f757f4d8c43062b4940a213722652
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-09 17:16:11 +0100

    fix

--- src/helpers/widgets/edges.py
+++ src/helpers/widgets/edges.py
@@ -3,12 +3,35 @@ from dataclasses import dataclass
 from typing import ClassVar
 
 
-class classproperty(object):
-    def __init__(self, f):
-        self.f = f
+class ClassPropertyDescriptor(object):
 
-    def __get__(self, obj, owner):
-        return self.f(owner)
+    def __init__(self, fget, fset=None):
+        self.fget = fget
+        self.fset = fset
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+        return self.fget.__get__(obj, klass)()
+
+    def __set__(self, obj, value):
+        if not self.fset:
+            raise AttributeError("can't set attribute")
+        type_ = type(obj)
+        return self.fset.__get__(obj, type_)(value)
+
+    def setter(self, func):
+        if not isinstance(func, (classmethod, staticmethod)):
+            func = classmethod(func)
+        self.fset = func
+        return self
+
+
+def classproperty(func):
+    if not isinstance(func, (classmethod, staticmethod)):
+        func = classmethod(func)
+
+    return ClassPropertyDescriptor(func)
 
 
 @dataclass
@@ -30,6 +53,7 @@ class Edges:
     def only(top: float = 0.0, bottom: float = 0.0, right: float = 0.0, left: float = 0.0) -> Edges:
         return Edges(top=top, bottom=bottom, right=right, left=left)
 
-    @classproperty
+    @property
+    @classmethod
     def zero(cls) -> Edges:
         return Edges()
