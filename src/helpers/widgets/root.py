37732312305e56d54ef0a2b8947f1549a2d0b036
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-09 16:33:09 +0100

    v

--- src/helpers/widgets/root.py
+++ src/helpers/widgets/root.py
@@ -12,7 +12,7 @@ class Root:
         self.y_0 = y_0
         self.x_1 = x_1
         self.y_1 = y_1
-        self.size = Size((x_1-x_0) * 119, (y_1-y_0) * 120)
+        self.size = Size((x_1-x_0) * 119.5, (y_1-y_0) * 120)
         self.offset = Offset(x_0 * 120, y_0 * 120)
         self.child = child
         super().__init__()
