1069a73a2c1fa52cae69a5327be6d49a5f74669b
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-09 20:54:08 +0100

    fix

--- src/helpers/widgets/text.py
+++ src/helpers/widgets/text.py
@@ -4,14 +4,14 @@ from PIL.ImageFont import truetype as font
 
 
 class Text(Widget):
-    def __init__(self, text: str, fontSize: float = 16, color: Color = Colors.white, fontType: str = "arial.ttf") -> None:
+    def __init__(self, text: str, fontSize: float = 16.0, color: Color = Colors.white, fontType: str = "arial.ttf") -> None:
         self.text = text
         self.fontSize = fontSize
         self.color = color
         self.fontType = fontType
-        self.size = Size(*font(fontType, fontSize).getsize(text))
+        self.size = Size(*font(self.fontType, int(self.fontSize)).getsize(self.text))
         super().__init__()
 
     def draw(self, canvas: ImageDraw, offset: Offset, max_size: Size, ratio: float) -> None:
-        self.font = font(self.fontType, self.fontSize * ratio)
+        self.font = font(self.fontType, int(self.fontSize * ratio))
         canvas.text((offset.dx*ratio, offset.dy*ratio), self.text, self.color.color)
