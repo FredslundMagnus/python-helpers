948921beb08682a4e62b8d4e77022849c26e9a4d
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-10 20:06:17 +0100

    fix

--- src/helpers/widgets/languages.py
+++ src/helpers/widgets/languages.py
@@ -44,9 +44,9 @@ class Language:
         if token in self.strings:
             return self.color_strings
         if token == Tokens.Name:
-            return self.color_booleans
+            return self.color_default
         if token == Tokens.Keyword.Constant:
-            return self.color_strings
+            return self.color_booleans
         if token == Tokens.Keyword:
             return self.color_strings
         if token == Tokens.Keyword.Namespace:
