c897cef2cd49340cc224c79c5b4e23fd44c60681
FredslundMagnus <s183905@student.dtu.dk>
2022-01-10 12:30:48 +0100

    fix

--- src/helpers/widgets/scale.py
+++ src/helpers/widgets/scale.py
@@ -9,4 +9,7 @@ class Scale(Widget):
 
     def draw(self, canvas: ImageDraw, offset: Offset, max_size: Size, ratio: float) -> None:
         if self.child is not None:
-            self.child.draw(canvas, offset, max_size, ratio * self.scale)
+            _offset = Offset(offset.dx * self.scale, offset.dy * self.scale)
+            _size = Size(max_size.width * self.scale, max_size.height * self.scale)
+            _ratio = ratio * self.scale
+            self.child.draw(canvas, _offset, _size, _ratio)
