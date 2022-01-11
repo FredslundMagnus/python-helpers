0e20c5e0cd93d1f7f2b8a1b659edb12d6d03499b
FredslundMagnus <s183905@student.dtu.dk>
2022-01-11 11:16:46 +0100

    fix

--- test.py
+++ test.py
@@ -18,7 +18,8 @@ if 1 is None:
     class Name:
         def test():
             pass
-aiter(), anext(), ascii(), breakpoint(), callable(), exec(), sorted(), help()
+sorted()
+aiter(), anext(), ascii(), breakpoint(), callable(), exec(), help()
 eval(), format(), getattr(), globals(), hasattr(), hash(), hex(), id(), input(), isinstance(), __import__()
 abs(), all(), any(), bin(), chr(), compile(), delattr(), dir(), divmod(), issubclass(), iter(), vars()
 len(), locals(), max(), min(), next(), oct(), open(), ord(), pow(), print(), repr(), round(), setattr(), sum()
