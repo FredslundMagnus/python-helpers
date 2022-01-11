e4bd75d01d6d52e3ed8ddf1fac57e279533a4ed9
FredslundMagnus <s183905@student.dtu.dk>
2022-01-11 12:52:46 +0100

    fix

--- src/helpers/widgets/code.py
+++ src/helpers/widgets/code.py
@@ -28,6 +28,12 @@ class Code(Widget):
         return Code(code, language=Languages.fromExtension(filename.split('.')[-1]), fontSize=fontSize, lineHeight=lineHeight, functions=functions, classes=classes)
 
     def draw(self, canvas: ImageDraw, offset: Offset, max_size: Size, ratio: float) -> None:
+        for fontsize in range(1, 1000):
+            size = Size(self.charWidth*max(len(line) for line in self.lines), fontsize*(len(self.lines) + (len(self.lines)-1)*(self.lineHeight-1.0)))
+            if size.width > max_size.width or size.height > max_size.height:
+                break
+        fontsize -= 1
+        font = self.font.pil(fontsize * ratio)
         for y, (line, colors) in enumerate(zip(self.lines, self.colors)):
             for x, (char, color) in enumerate(zip(line, colors)):
-                canvas.text(((offset.dx + x*self.charWidth)*ratio, (offset.dy + y*self.fontSize*self.lineHeight)*ratio), char, color.color, self.font.pil(self.fontSize * ratio))
+                canvas.text(((offset.dx + x*self.charWidth)*ratio, (offset.dy + y*self.fontSize*self.lineHeight)*ratio), char, color.color, font)
