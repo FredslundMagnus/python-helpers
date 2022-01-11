d478db54ce9409ea018aa0dcd292df43292bf7bc
FredslundMagnus <s183905@student.dtu.dk>
2022-01-11 10:37:25 +0100

    fix

--- test.py
+++ test.py
@@ -19,10 +19,10 @@ if 1 is None:
         def test():
             pass
 aiter(), anext(), ascii(), breakpoint(), callable(), exec(), help()
-abs(), all(), any(), bin(), chr(), compile(), delattr(), dir(), divmod()
+abs(), all(), any(), bin(), chr(), compile(), delattr(), dir(), divmod(), issubclass(), iter()
 bool(), bytearray(), bytes(), classmethod(), complex(), dict()
-enumerate(), eval(), filter(), float(), format(), frozenset(), getattr(), globals()
-hasattr(), hash(), hex(), id(), input(), int(), isinstance(), issubclass(), iter()
+enumerate(), filter(), float(), frozenset(), int()
+eval(), format(), getattr(), globals(), hasattr(), hash(), hex(), id(), input(), isinstance()
 len(), list(), locals(), map(), max(), memoryview(), min(), next(), object(), oct()
 open(), ord(), pow(), print(), property(), range(), repr(), reversed(), round(), set(), setattr()
 slice(), sorted(), staticmethod(), str(), sum(), super(), tuple(), type(), vars(), zip(), __import__()
