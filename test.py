be61bd4a7cf54f0bce2f68a1a5da9d2820e861ab
FredslundMagnus <s183905@student.dtu.dk>
2022-01-11 10:15:29 +0100

    fix

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import cv2
 import numpy as np
-import struct
 from numpy import arange, Tester
-
 outputVideo = cv2.VideoWriter()
 outputVideo.open("foo.avi", -1, 25, (640, 480), True)
 frame = np.zeros((480, 640, 3), dtype=np.uint8)
@@ -11,7 +8,6 @@ int(), float(), bool(), range(), enumerate(), zip(), max(), abs(), sum()
 print("FOURCC is '%s'" % struct.pack("<I", fourcc), fourcc)
 while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from
 # sdfg while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from
-"while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from"
 """while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from"""
 2.1, 3, 1
 
@@ -23,3 +19,10 @@ if 1 is None:
     class Name:
         def test():
             pass
+abs(), aiter(), all(), any(), anext(), ascii(), bin(), bool(), breakpoint(), bytearray(), bytes(),
+callable(), chr(), classmethod(), compile(), complex(), delattr(), dict(), dir(), divmod()
+enumerate(), eval(), exec(), filter(), float(), format(), frozenset(), getattr(), globals()
+hasattr(), hash(), help(), hex(), id(), input(), int(), isinstance(), issubclass(), iter()
+len(), list(), locals(), map(), max(), memoryview(), min(), next(), object(), oct()
+open(), ord(), pow(), print(), property(), range(), repr(), reversed(), round(), set(), setattr()
+slice(), sorted(), staticmethod(), str(), sum(), super(), tuple(), type(), vars(), zip(), __import__()
