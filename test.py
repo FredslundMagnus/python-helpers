034d0f317454604abc5c8766113849b6a5953231
FredslundMagnus <s183905@student.dtu.dk>
2022-01-11 09:39:02 +0100

    fix

--- test.py
+++ test.py
@@ -5,19 +5,15 @@ from numpy import arange, Tester
 
 outputVideo = cv2.VideoWriter()
 outputVideo.open("foo.avi", -1, 25, (640, 480), True)
-
 frame = np.zeros((480, 640, 3), dtype=np.uint8)
 
-
+int(), float(), bool(), range(), enumerate(), zip(), max(), abs(), sum()
 print("FOURCC is '%s'" % struct.pack("<I", fourcc), fourcc)
 while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from
 # sdfg while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from
 "while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from"
-'while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from'
-'''while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from'''
 """while, if, return, for, else, raise, pass, break, try, except, yield, continue, assert, import, as, from"""
-2.1
-3, 1
+2.1, 3, 1
 
 if 1 in {2, 1}:
     pass
