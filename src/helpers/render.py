d453ef0aed9f1b5128d9d8c1a2d6255397c45276
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-16 20:41:43 +0100

    fix

--- src/helpers/render.py
+++ src/helpers/render.py
@@ -175,7 +175,7 @@ def calculate_highlight(x: float, y: float, z: float) -> tuple[float, float, flo
 
 @njit
 def _interpolate(pixel_color: tuple[int, int, int], value: float) -> tuple[int, int, int]:
-    return [int(p*(0.1 + 0.9*value)) for p in pixel_color]
+    return (*[int(p*(0.1 + 0.9*value)) for p in pixel_color], 255)
     # return tuple(int((v2 - v1)*x+v1) for v1, v2 in zip(color1, color2))
 
 
@@ -195,7 +195,7 @@ def drawBackground(canvas: ImageDraw.ImageDraw, pixel_color: Color, size: tuple[
             # color = getColor(pixel_color.color, cos_angle*intencity)
             # color = Color.interpolate(Colors.black, pixel_color,  cos_angle*intencity*0.9+0.1).color
             color = _interpolate(pixel_color.color, cos_angle*intencity)
-            canvas.point((x, y), (*color, 255))
+            canvas.point((x, y), color)
 
 
 def drawBox(canvas: ImageDraw.ImageDraw, box: tuple[float, float, float, float, Color], size: tuple[int, int]) -> None:
@@ -210,7 +210,7 @@ def drawBox(canvas: ImageDraw.ImageDraw, box: tuple[float, float, float, float, 
             cos_angle = calculate_highlight(_x, _y, z)
             # color = Color.interpolate(Color.interpolate(pixel_color, Colors.black, 0.9), pixel_color,  cos_angle*intencity).color
             color = _interpolate(pixel_color.color, cos_angle*intencity)
-            canvas.point((x, y), (*color, 255))
+            canvas.point((x, y), color)
 
 
 @njit
