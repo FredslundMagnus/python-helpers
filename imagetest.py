a81bbc4224cd5426980db7f471a54e4e6fcbace3
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-16 17:37:49 +0100

    fix

--- imagetest.py
+++ imagetest.py
@@ -1,69 +1,10 @@
 from __future__ import annotations
-import cv2
-from src.helpers.curves import Curves
 from src.helpers.colors import Color, Colors
-from src.helpers.image.background import Background
+from src.helpers.render import Background, render_image, render_video
 from src.helpers.widgets.widgets import *
-from PIL import Image
-import numpy as np
-from enum import Enum
+from time import time
 
 
-class VideoFormat(Enum):
-    mov = 4
-    avi = 0
-
-
-def create_video(name: str, files: list[Background], fps: int = 60, size: tuple[int, int] = (1920, 1080), test: bool = False, videoFormat: VideoFormat = VideoFormat.mov):
-    video = cv2.VideoWriter(f'Videos/{name}.{videoFormat.name.split(".")[-1]}', videoFormat.value, fps, size)
-
-    for i, image in enumerate(files, start=1):
-        print(f"{i} out of {len(files)}")
-        if not test:
-            image.load(size=size)
-        background = Image.new(mode="RGBA", size=size, color=(*image.color.color, 255)) if test else Image.open(image.name(size=size))
-        for img in (root.draw(size=size, test=test) for root in image.children):
-            background.paste(img, (0, 0), img)
-
-        pil_image = np.array(background.convert('RGB'))
-        open_cv_image = pil_image[:, :, ::-1].copy()
-        video.write(open_cv_image)
-
-    cv2.destroyAllWindows()
-    video.release()
-
-
-def create_image(name: str, file: Background, size: tuple[int, int] = (1920, 1080), test: bool = False):
-    if not test:
-        file.load(size=size)
-    background = Image.new(mode="RGBA", size=size, color=(*file.color.color, 255)) if test else Image.open(file.name(size=size))
-    for img in (root.draw(size=size, test=test) for root in file.children):
-        background.paste(img, (0, 0), img)
-    background.save(f"Images/{name}.png")
-
-
-# Background(color=Colors.brown, boxes=[(0, 0, 16, 9)]).load(size=(1920, 1080))
-
-# Background(color=Colors.brown, boxes=[(16/9, 1, 16-16/9, 8)]).load(size=(1920, 1080))
-
-# Background(color=Colors.brown, boxes=[(1, 1, 7.5, 8, Colors.green), (8.5, 1, 15, 8, Colors.red)]).load(size=(1920, 1080))
-
-# Background(color=Colors.brown, boxes=[(1, 1, 7.5, 8, Colors.blue), (8.5, 1, 15, 8)]).load(size=(1920, 1080))
-
-# Background(color=Colors.brown, boxes=[(1, 1, 5, 8, Colors.blue), (6, 1, 10, 8), (11, 1, 15, 8, Colors.green)]).load(size=(1920, 1080))
-
-# trans = Background.transition(
-#     Background(color=Colors.brown, boxes=[(1, 1, 5, 8, Colors.blue), (6, 1, 10, 8), (11, 1, 15, 8, Colors.green)]),
-#     Background(color=Colors.yellow, boxes=[(1, 1, 5, 8, Colors.green), (6, 1, 10, 8, Colors.red), (11, 1, 15, 8)]),
-#     frames=10,
-#     curve=Curves.linear,
-# )
-# trans2 = Background.transition(
-#     Background(color=Colors.brown, boxes=[(1, 1, 5, 8, Colors.blue), (6, 1, 10, 8), (11, 1, 15, 8, Colors.green)]),
-#     Background(color=Colors.yellow, boxes=[(1, 1, 7.5, 8, Colors.green), (8.5, 1, 15, 8, Colors.red), (17, 1, 30, 8)]),
-#     frames=40,
-#     curve=Curves.linear,
-# )
 children = [
     FileEditor(
         filename="example.py",
@@ -94,7 +35,6 @@ test1 = Background.transition(
     Background(color=Colors.blue, boxes=[(1, 1, 5, 8), (6, 1, 10, 8), (11, 1, 15, 8)]),
     Background(color=Colors.blue, boxes=[(1, 1, 7.5, 8), (8.5, 1, 15, 8), (17, 1, 30, 8)]),
     frames=40,
-    curve=Curves.easeInOut,
     children=children,
 )
 
@@ -104,18 +44,16 @@ test3 = Background.transition(
     Background(color=Colors.blue, boxes=[(1, 1, 7.5, 8), (8.5, 1, 15, 8)]),
     Background(color=Colors.blue, boxes=[(16/9, 1, 16-16/9, 8), (17, 1, 30, 8)]),
     frames=40,
-    curve=Curves.easeInOut,
     children=children,
 )
 
 test4 = [Background(color=Colors.blue, boxes=[(16/9, 1, 16-16/9, 8)], children=children) for _ in range(40)]
 
-# test5 = Background.transition(
-#     Background(color=Colors.blue, boxes=[(16/9, 1, 16-16/9, 8), (17, 1, 30, 8)]),
-#     Background(color=Colors.blue, boxes=[(1, 1, 7.5, 8), (8.5, 1, 15, 8)]),
-#     frames=40,
-#     curve=Curves.easeInOut,
-# )
+test5 = Background.transition(
+    Background(color=Colors.blue, boxes=[(16/9, 1, 16-16/9, 8), (17, 1, 30, 8)]),
+    Background(color=Colors.blue, boxes=[(1, 1, 7.5, 8), (8.5, 1, 15, 8)]),
+    frames=40,
+)
 
 
 def ani6(animation: float) -> list[Widget]:
@@ -137,7 +75,6 @@ test6 = Background.transition(
     Background(color=Colors.blue, boxes=[box(height=3.5, center_x=8.0-4.0+16/9/4), box(height=3.5, center_x=8.0+4.0-16/9/4)]),
     Background(color=Colors.blue, boxes=[box(height=7.0), box(height=7.0, center_x=8.0+16.0)]),
     frames=40,
-    curve=Curves.easeInOut,
     children=ani6,
 )
 
@@ -145,7 +82,6 @@ test7 = Background.transition(
     Background(color=Colors.blue, boxes=[box(height=3.5, center_x=8.0-4.0+16/9/4), box(height=3.5, center_x=8.0+4.0-16/9/4)]),
     Background(color=Colors.blue, boxes=[box(height=7.0, center_x=8.0-16.0), box(height=7.0)]),
     frames=40,
-    curve=Curves.easeInOut,
     children=ani6,
 )
 
@@ -157,7 +93,6 @@ def test10(color): return Background.transition(
     Background(color=color, boxes=[(1, 1, 10, 8), (11, 1, 15, 8, Colors.gray.c900)]),
     Background(color=color, boxes=[(16/9, 1, 16-16/9, 8), (17, 1, 21, 8, Colors.gray.c900)]),
     frames=40,
-    curve=Curves.easeInOut,
     children=children,
 )
 
@@ -167,6 +102,7 @@ def big(color): return [Background(color=color, boxes=[(16/9, 1, 16-16/9, 8), (1
 
 
 test: bool = False
+size = (1920, 1080)
 idea = list(reversed(test6)) + test7 + list(reversed(test7)) + test6
 
 
@@ -174,34 +110,33 @@ def textConsoleSplit(color: Color) -> Background:
     return Background(color=color, boxes=[(1, 1, 10, 8), (11, 1, 15, 8, Colors.gray.c900)], children=children)
 
 
-if test:
-    # create_video("testHD", test0[:20], size=(1920*2, 1080*2), test=test)
-    # create_image("test0Container", test0[0], size=(1920*2, 1080*2), test=test)
-    # create_video("test6HD", idea, size=(1920*2, 1080*2), test=test)
-    create_video("textAndConsole4kTest", (big(Colors.green) + list(reversed(test10(Colors.green))) + small(Colors.green) + test10(Colors.green))*3, size=(1920*2, 1080*2), test=test)
-
-else:
-    # create_image("testLang", test4[0], size=(1920*2, 1080*2), test=test)
-    # create_image("withConsole1", test8, size=(1920*2, 1080*2), test=test)
-    # create_image("withConsole2", test9, size=(1920*2, 1080*2), test=test)
-    # create_image("redTextConsole", textConsoleSplit(Colors.red), size=(1920*2, 1080*2), test=test)
-    # create_image("deepOrangeTextConsole", textConsoleSplit(Colors.deepOrange), size=(1920*2, 1080*2), test=test)
-    # create_image("orangeTextConsole", textConsoleSplit(Colors.orange), size=(1920*2, 1080*2), test=test)
-    create_image("yellowTextConsole", textConsoleSplit(Colors.yellow), size=(1920*2, 1080*2), test=test)
-    # create_image("brownTextConsole", textConsoleSplit(Colors.brown), size=(1920*2, 1080*2), test=test)
-    create_video("textAndConsole4kNewGreen", (big(Colors.green) + list(reversed(test10(Colors.green))) + small(Colors.green) + test10(Colors.green))*3, size=(1920*2, 1080*2), test=test)
-    create_video("textAndConsole4kNewRed", (big(Colors.red) + list(reversed(test10(Colors.red))) + small(Colors.red) + test10(Colors.red))*3, size=(1920, 1080), test=test)
-    # create_video("test6HD4", idea, size=(1920, 1080), test=test, fps=30)
-    # create_video("test4k", (test0 + test1 + test2 + test3 + test4 + list(reversed(test3)) + test2 + list(reversed(test1))) * 3, size=(1920*2, 1080*2))
-    # create_image("test0Container", test0[0], size=(1920*2, 1080*2))
-    # create_image("test1Container", test2[0], size=(1920*2, 1080*2))
-    # create_image("test2Container", test4[0], size=(1920*2, 1080*2))
-# create_image("test0ContainerSmall", test0[0], size=(1920, 1080))
-# create_image("test1ContainerSmall", test2[0], size=(1920, 1080))
-# create_image("test2ContainerSmall", test4[0], size=(1920, 1080))
-# [tran.load() for tran in trans]
-# [tran.load() for tran in trans2]
+render_video(f"textAndConsole4kTestRender{test}", (big(Colors.green) + list(reversed(test10(Colors.green))) + small(Colors.green) + test10(Colors.green))*3, size=(1920*2, 1080*2), test=test)
 
 
-# make("test", trans, fps=5)
-# make("test2", trans2, fps=5)
+# render_image("testLang", test4[0], size=(1920*2, 1080*2), test=test)
+# render_image("withConsole1", test8, size=(1920*2, 1080*2), test=test)
+# render_image("withConsole2", test9, size=(1920*2, 1080*2), test=test)
+# render_image("redTextConsole", textConsoleSplit(Colors.red), size=(1920*2, 1080*2), test=test)
+# render_image("deepOrangeTextConsole", textConsoleSplit(Colors.deepOrange), size=(1920*2, 1080*2), test=test)
+# render_image("orangeTextConsole", textConsoleSplit(Colors.orange), size=(1920*2, 1080*2), test=test)
+# render_image("yellowTextConsoleRender", textConsoleSplit(Colors.yellow), size=(1920*2, 1080*2), test=test)
+# render_image("brownTextConsole", textConsoleSplit(Colors.brown), size=(1920*2, 1080*2), test=test)
+# render_video("textAndConsole4kNewGreenRender", (big(Colors.green) + list(reversed(test10(Colors.green))) + small(Colors.green) + test10(Colors.green))*3, size=(1920*2, 1080*2), test=test)
+
+start = time()
+render_video("textAndConsole4kNewPinkRenderSmall", (big(Colors.pink) + list(reversed(test10(Colors.pink)))[:10]), size=(1920, 1080), test=test)
+end = time()
+print(end - start)  # 101.64646553993225
+start = time()
+render_video("textAndConsole4kNewIndigoRenderSmall", (big(Colors.indigo) + list(reversed(test10(Colors.indigo)))[:10]), size=(1920, 1080), test=test)
+end = time()
+print(end - start)  # 18.415156364440918
+
+# render_video("test6HD4", idea, size=(1920, 1080), test=test, fps=30)
+# render_video("test4k", (test0 + test1 + test2 + test3 + test4 + list(reversed(test3)) + test2 + list(reversed(test1))) * 3, size=(1920*2, 1080*2))
+# render_image("test0Container", test0[0], size=(1920*2, 1080*2))
+# render_image("test1Container", test2[0], size=(1920*2, 1080*2))
+# render_image("test2Container", test4[0], size=(1920*2, 1080*2))
+# render_image("test0ContainerSmall", test0[0], size=(1920, 1080))
+# render_image("test1ContainerSmall", test2[0], size=(1920, 1080))
+# render_image("test2ContainerSmall", test4[0], size=(1920, 1080))
---
+++ src/helpers/render.py
@@ -0,0 +1,305 @@
+from __future__ import annotations
+from PIL import Image, ImageDraw, ImageFilter
+from helpers.colors import Colors, Color
+from helpers.widgets.widget import Widget
+from helpers.widgets.root import Root
+from helpers.curves import Curve, Curves
+import numpy as np
+from numba import njit
+from time import time
+from os.path import join, exists
+from functools import lru_cache  # python 3.9 has cache wich is faster for maxsize=None
+from PIL import Image
+import numpy as np
+from enum import Enum
+import cv2
+
+name = "your_file.png"
+intencity = 0.9
+u_Shininess = 10.0
+u_Light_position = np.array([16/9*2.3, -0.5*5, 2*8])
+# print(u_Light_position)
+camera = np.array([0.5, 0.5, 2])
+# size = (3840, 2160)
+blur = 8
+factor = None
+# background = Colors.red
+# box1: tuple[float, float, float, float, Color] = (1.0, 1.0, 10.0, 8.0, Colors.gray.c800)
+# box2: tuple[float, float, float, float, Color] = (11.0, 1.0, 15.0, 8.0, Colors.gray.c900)
+# boxes: list[tuple[float, float, float, float, Color]] = [box1, box2]
+
+
+class Background:
+    def __init__(self, color: Color, boxes: list[tuple[float, float, float, float] | tuple[float, float, float, float, Color]] = [], box_color: Color = Colors.gray.c800, folder: str = "BackgroundsNew", children: list[Widget] = []) -> None:
+        self.folder = folder
+        self.color = color
+        self.color_name = str(self.color.color)[1: -1].replace(', ', '_')
+        self.boxes: list[tuple[float, float, float, float, Color]] = [(tuple(list(b) + [box_color]) if len(b) == 4 else b) for b in boxes]
+        self.children = [Root(*box[:4], child) for child, box in zip(children, boxes)]
+
+    @property
+    def boxes_name(self) -> list[str]:
+        return "_".join([f"{self.fix(b[0])}-{self.fix(b[1])}-{self.fix(b[2])}-{self.fix(b[3])}-" + str(b[4].color)[1: -1].replace(', ', '_') for b in self.boxes])
+
+    def fix(self, pos: float, x: bool = True) -> float:
+        b = -6.94 if x else 3.9
+        a = 0.867
+        return int((a*pos + b) * 10000) / 10000
+
+    def name(self, size: tuple[int, int] = (1920, 1080)) -> str:
+        width, height = size
+        return join(self.folder, f"full-{self.color_name}-{width}-{height}-{self.boxes_name}.png")
+
+    def empty_name(self, size: tuple[int, int] = (1920, 1080)) -> str:
+        width, height = size
+        print(exists("Empty"))
+        return join("Empty", f"background-{self.color_name}-{width}-{height}.png")
+
+    def rendered_boxes_name(self, size: tuple[int, int] = (1920, 1080)) -> str:
+        width, height = size
+        print(exists("Boxes"))
+        return join("Boxes", f"boxes-{width}-{height}-{self.boxes_name}.png")
+
+    def render(self, size: tuple[int, int] = (1920, 1080)) -> None:
+        empty_name = self.empty_name(size)
+        if not exists(empty_name):
+            im = Image.new("RGBA", size)
+            canvas = ImageDraw.Draw(im)
+
+            drawBackground(canvas, self.color, size)
+
+            im.save(empty_name)
+        else:
+            im = Image.open(empty_name)
+
+        boxes_name = self.rendered_boxes_name(size)
+        if not exists(boxes_name):
+            im2 = Image.new("RGBA", size)
+            canvas2 = ImageDraw.Draw(im2)
+
+            for box in self.boxes:
+                drawBoxShadow(im2, box, size)
+
+            for box in self.boxes:
+                drawBoxBorder(canvas2, box)
+
+            for box in self.boxes:
+                drawBox(canvas2, box, size)
+
+            im2.save(boxes_name)
+        else:
+            im2 = Image.open(boxes_name)
+
+        im.alpha_composite(im2, (0, 0))
+        im.save(self.name(size))
+        return im
+
+    def load(self, size: tuple[int, int] = (1920, 1080)):
+        global factor
+        factor = size[0] / 16
+        name = self.name(size)
+        if not exists(name):
+            return self.render(size=size)
+        return Image.open(name)
+
+    @staticmethod
+    def transition(background1: Background, background2: Background, frames: int, curve: Curve = Curves.easeInOut, children: list[Widget] = []) -> list[Background]:
+        backgrounds: list[Background] = []
+        for step in (curve(i/(frames-1)) for i in range(frames)):
+            boxes: list[tuple[float, float, float, float, Color]] = []
+            for box1, box2, in zip(background1.boxes, background2.boxes):
+                *vs1, c1 = box1
+                *vs2, c2 = box2
+                box = tuple([(v2-v1)*step + v1 for v1, v2 in zip(vs1, vs2)] + [Colors.interpolate(c1, c2, step)])
+                boxes.append(box)
+            color = Colors.interpolate(background1.color, background2.color, step)
+            try:
+                _children = children(step)
+            except Exception:
+                _children = children
+            backgrounds.append(Background(color=color, boxes=boxes, children=_children))
+        return backgrounds
+
+
+@njit
+def normalize(v):
+    return v / np.sqrt(np.sum(v**2))
+
+
+@lru_cache(maxsize=None)
+@njit
+def calculate_color(x: float, y: float, z: float) -> tuple[float, float, float]:
+    # Calculate a vector from the fragment location to the light source
+    v_Vertex = np.array([x, y, z])
+    v_Normal = np.array([0, 0, 1])
+
+    to_light = u_Light_position - v_Vertex
+    to_light = normalize(to_light)
+
+    # The vertex's normal vector is being interpolated across the primitive
+    # which can make it un-normalized. So normalize the vertex's normal vector.
+    vertex_normal = normalize(v_Normal)
+
+    # Calculate the reflection vector
+    reflection = 2.0 * np.dot(vertex_normal, to_light) * vertex_normal - to_light
+
+    # Calculate a vector from the fragment location to the camera.
+    # The camera is at the origin, so negating the vertex location gives the vector
+    to_camera = camera - v_Vertex
+
+    # Calculate the cosine of the angle between the reflection vector
+    # and the vector going to the camera.
+    reflection = normalize(reflection)
+    to_camera = normalize(to_camera)
+    cos_angle = np.dot(reflection, to_camera)
+    if cos_angle < 0.0:
+        cos_angle = 0.0
+    if cos_angle > 1.0:
+        cos_angle = 1.0
+    # cos_angle = np.clip(cos_angle, 0.0, 1.0)
+    cos_angle = pow(cos_angle, u_Shininess)
+
+    #   // If this fragment gets a specular reflection, use the light's color,
+    #   // otherwise use the objects's color
+    #   specular_color = u_Light_color * cos_angle;
+    #   object_color = vec3(v_Color) * (1.0 - cos_angle);
+    #   color = specular_color + object_color;
+    return cos_angle
+    # return Color.interpolate(color, Colors.white,  cos_angle).color
+
+
+def drawBackground(canvas: ImageDraw.ImageDraw, pixel_color: Color, size: tuple[int, int]):
+    z = -2.0
+    for y in range(size[1]):
+        _y = y / (size[1] - 1)
+        for x in range(size[0]):
+            _x = x / (size[1] - 1)
+            cos_angle = calculate_color(_x, _y, z)
+            color = Color.interpolate(Color.interpolate(pixel_color, Colors.black, 0.9), pixel_color,  cos_angle*intencity).color
+            canvas.point((x, y), (*color, 255))
+
+
+@njit
+def fixX(x: float) -> float:
+    return (x-14)*1.015 + 14
+
+
+@njit
+def fixY(y: float) -> float:
+    return (y-0)*1.02 + 0.07
+
+
+@njit
+def centerX(x: float) -> float:
+    return (x-8)*0.988 + 8
+
+
+@njit
+def centerY(y: float) -> float:
+    return (y-4.5)*0.988 + 4.5
+
+
+@njit
+def colorX(x: float) -> float:
+    return abs(x-14)/9/2
+
+
+@njit
+def colorY(y: float) -> float:
+    return abs(y-0)/9/2
+
+
+def edgeShadow(canvas: ImageDraw.ImageDraw, corner1: tuple[float, float], corner2: tuple[float, float], color: tuple[int, int, int, int]) -> None:
+    canvas.polygon((
+        factor * fixX(corner1[0]), factor * fixY(corner1[1]),
+        factor * fixX(corner2[0]), factor * fixY(corner2[1]),
+        factor * fixX(centerX(corner2[0])), factor * fixY(centerY(corner2[1])),
+        factor * fixX(centerX(corner1[0])), factor * fixY(centerY(corner1[1])),
+    ), color)
+
+
+def edge(canvas: ImageDraw.ImageDraw, corner1: tuple[float, float], corner2: tuple[float, float], color: tuple[int, int, int, int]) -> None:
+    canvas.polygon((
+        factor * corner1[0], factor * corner1[1],
+        factor * corner2[0], factor * corner2[1],
+        factor * centerX(corner2[0]), factor * centerY(corner2[1]),
+        factor * centerX(corner1[0]), factor * centerY(corner1[1]),
+    ), color)
+
+
+def drawEdges(canvas: ImageDraw.ImageDraw, box: tuple[float, float, float, float, Color], color: tuple[int, int, int, int]) -> None:
+    if (box[0] > 8):
+        edgeShadow(canvas, (box[0], box[1]), (box[0], box[3]), color)
+    if (box[1] > 4.5):
+        edgeShadow(canvas, (box[0], box[1]), (box[2], box[1]), color)
+    if (box[3] < 4.5):
+        edgeShadow(canvas, (box[2], box[3]), (box[0], box[3]), color)
+    if (box[2] < 8):
+        edgeShadow(canvas, (box[2], box[3]), (box[2], box[1]), color)
+
+
+def drawBoxShadow(im: Image.Image, box: tuple[float, float, float, float, Color], size: tuple[int, int]) -> None:
+    shadow = Image.new("RGBA", size, (0, 0, 0, 0))
+    shadowBox = ImageDraw.Draw(shadow)
+    shadowBox.rectangle((size[0] / 16 * fixX(box[0]), size[1] / 9 * fixY(box[1]), size[0] / 16 * fixX(box[2]), size[1] / 9 * fixY(box[3])), (0, 0, 0, 220))
+    drawEdges(shadowBox, box, (0, 0, 0, 220))
+    shadow = shadow.filter(ImageFilter.GaussianBlur(radius=size[1] / 900 * blur))
+    im.alpha_composite(shadow, (0, 0))
+
+
+def drawBoxBorder(canvas: ImageDraw.ImageDraw, box: tuple[float, float, float, float, Color]) -> None:
+    if (box[0] > 8):
+        edge(canvas, (box[0], box[1]), (box[0], box[3]), (*Color.interpolate(Colors.black, box[4], colorX(box[0])).color, 255))
+    if (box[1] > 4.5):
+        edge(canvas, (box[0], box[1]), (box[2], box[1]), (*Color.interpolate(Colors.black, box[4], colorY(box[1])).color, 255))
+    if (box[3] < 4.5):
+        edge(canvas, (box[2], box[3]), (box[0], box[3]), (*Color.interpolate(Colors.black, box[4], colorY(box[3])).color, 255))
+    if (box[2] < 8):
+        edge(canvas, (box[2], box[3]), (box[2], box[1]), (*Color.interpolate(Colors.black, box[4], colorX(box[2])).color, 255))
+
+
+def drawBox(canvas: ImageDraw.ImageDraw, box: tuple[float, float, float, float, Color], size: tuple[int, int]) -> None:
+    z = -1.9
+    pixel_color = box[4]
+    for y in range(size[1]):
+        _y = y / (size[1] - 1)
+        for x in range(size[0]):
+            if not (factor * box[0] < x < factor * box[2] and factor * box[1] < y < factor * box[3]):
+                continue
+            _x = x / (size[1] - 1)
+            cos_angle = calculate_color(_x, _y, z)
+            color = Color.interpolate(Color.interpolate(pixel_color, Colors.black, 0.9), pixel_color,  cos_angle*intencity).color
+            canvas.point((x, y), (*color, 255))
+
+
+class VideoFormat(Enum):
+    mov = 4
+    avi = 0
+
+
+def render_video(name: str, files: list[Background], fps: int = 60, size: tuple[int, int] = (1920, 1080), test: bool = False, videoFormat: VideoFormat = VideoFormat.mov):
+    video = cv2.VideoWriter(f'Videos/{name}.{videoFormat.name.split(".")[-1]}', videoFormat.value, fps, size)
+
+    for i, image in enumerate(files, start=1):
+        print(f"{i} out of {len(files)}")
+        if not test:
+            image.load(size=size)
+        background = Image.new(mode="RGBA", size=size, color=(*image.color.color, 255)) if test else Image.open(image.name(size=size))
+        for img in (root.draw(size=size, test=test) for root in image.children):
+            background.paste(img, (0, 0), img)
+
+        pil_image = np.array(background.convert('RGB'))
+        open_cv_image = pil_image[:, :, ::-1].copy()
+        video.write(open_cv_image)
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+def render_image(name: str, file: Background, size: tuple[int, int] = (1920, 1080), test: bool = False):
+    if not test:
+        file.load(size=size)
+    background = Image.new(mode="RGBA", size=size, color=(*file.color.color, 255)) if test else Image.open(file.name(size=size))
+    for img in (root.draw(size=size, test=test) for root in file.children):
+        background.paste(img, (0, 0), img)
+    background.save(f"Images/{name}.png")
