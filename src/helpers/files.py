f0ceea45b06924931e209a06f0f243e7bfd0ccef
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-16 18:09:23 +0100

    fix

--- src/helpers/files.py
+++ src/helpers/files.py
@@ -2,15 +2,31 @@ from os import mkdir
 from os.path import exists
 
 
+class GitIgnore:
+    file: str = ".gitignore"
+
+    @property
+    @staticmethod
+    def exists() -> bool:
+        return exists(GitIgnore.file)
+
+    @staticmethod
+    def generate() -> None:
+        GitIgnore.add("*pyc")
+        GitIgnore.add("__pycache__\n")
+
+    @staticmethod
+    def add(line) -> None:
+        if not GitIgnore.exists:
+            with open(GitIgnore.file, "w") as f:
+                f.write(f"{line}\n")
+
+
+
+
 def makeSureFolderExists(name: str, gitignore: bool = False) -> str:
     if not exists(name):
         mkdir(name)
-    if not gitignore:
-        return name
-    with open(".gitignore", "a+") as f:
-        for line in f:
-            print(line, line == name+'/*', name+'/*')
-            # if line == name+'/*':
-            #     print()
-        # f.write(name+'/*')
+    if gitignore:
+        GitIgnore.add(name + '/*')
     return name
