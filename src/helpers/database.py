d807206d29b214fa64f4b5dc9ad0caf047aed7ed
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-01-23 19:59:53 +0100

    fix

--- src/helpers/database.py
+++ src/helpers/database.py
@@ -6,57 +6,61 @@ from google.cloud.firestore_v1.client import Client
 import os
 import json
 
+
 def json_files() -> list[str]:
-	temp = []
-	for folder in os.environ["Path"].split(';'):
-		try:
-			temp.extend([os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".json") and f.count("firebase")])
-		except Exception:
-			pass
-	return temp
+    temp = []
+    for folder in os.environ["Path"].split(';'):
+        try:
+            temp.extend([os.path.join(folder, f) for f in os.listdir(folder) if f.endswith(".json") and f.count("firebase")])
+        except Exception:
+            pass
+    return temp
+
 
 def get_credentials_path(project_id: str) -> str:
-	for file in json_files():
-		with open(file, 'r') as f:
-			if json.load(f).get('project_id', None) == project_id:
-				return file 
+    for file in json_files():
+        with open(file, 'r') as f:
+            if json.load(f).get('project_id', None) == project_id:
+                return file
+
 
 class Database:
-	def __init__(self, project_id: str, collection: str | None = None) -> None:
-		self.__project_id__ = project_id
-		self.__collection__ = collection
-		if collection is not None:
-			self.__create__()
-		
-	def __create__(self, collection: str | None = None):
-		if collection is not None:
-			self.__collection__ = collection
-		self.__database_url__ = f'https://{self.__project_id__}.firebaseio.com'
-		self.__credentials_path__ = get_credentials_path(self.__project_id__)
-		self.__credentials__ = Firebase.credentials.Certificate(self.__credentials_path__)
-		self.__app__: App = Firebase.initialize_app(self.__credentials__, {'databaseURL':self.__database_url__, 'project_id': self.__project_id__})
-		self.__database__: Client = firestore.client()
+    def __init__(self, project_id: str, collection: str | None = None) -> None:
+        self.__project_id__ = project_id
+        self.__collection__ = collection
+        if collection is not None:
+            self.__create__()
 
-	def collection(self, collection: str | None = None):
-		return self.__database__.collection(self.__collection__) if collection is None else self.__database__.collection(collection)
+    def __create__(self, collection: str | None = None):
+        if collection is not None:
+            self.__collection__ = collection
+        self.__database_url__ = f'https://{self.__project_id__}.firebaseio.com'
+        self.__credentials_path__ = get_credentials_path(self.__project_id__)
+        self.__credentials__ = Firebase.credentials.Certificate(self.__credentials_path__)
+        self.__app__: App = Firebase.initialize_app(self.__credentials__, {'databaseURL': self.__database_url__, 'project_id': self.__project_id__})
+        self.__database__: Client = firestore.client()
 
-	def set(self, document: str, data: dict, collection: str | None = None) -> None:
-		self.collection(collection).document(document).set(data)
+    def collection(self, collection: str | None = None):
+        return self.__database__.collection(self.__collection__) if collection is None else self.__database__.collection(collection)
 
-	def get(self,  document: str, collection: str | None = None) -> dict[str, object] | None:
-		return self.collection(collection).document(document).get().to_dict()
+    def set(self, document: str | None, data: dict, collection: str | None = None):
+        doc = self.collection(collection).document(document)
+        doc.set(data)
+        return doc
 
-	def getAll(self, collection: str | None = None):
-		for doc in self.collection(collection).stream():
-			yield doc.to_dict()
+    def get(self,  document: str, collection: str | None = None) -> dict[str, object] | None:
+        return self.collection(collection).document(document).get().to_dict()
 
+    def getAll(self, collection: str | None = None):
+        for doc in self.collection(collection).stream():
+            yield doc.to_dict()
 
 
 if __name__ == "__main__":
-	database = Database("dtu-server-test", "Test2")
-	
-	database.set("doc1", {"tal": 8, "bool": False, "Str": "sf", "float": 4.3})
-	print(database.get("doc1"))
+    database = Database("dtu-server-test", "Test2")
 
-	for v in database.getAll("Test"):
-		print(v)
+    database.set("doc1", {"tal": 8, "bool": False, "Str": "sf", "float": 4.3})
+    print(database.get("doc1"))
+
+    for v in database.getAll("Test"):
+        print(v)
