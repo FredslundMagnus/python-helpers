6fd459c847d44faef865e3805daea5cf8e91e279
FredslundMagnus <s183905@student.dtu.dk>
2022-02-01 11:18:02 +0100

    fix

--- src/helpers/discrete.py
+++ src/helpers/discrete.py
@@ -1,4 +1,7 @@
-from numpy import array as _array
+from numpy import array as _array, r_, nonzero, ones
+import numpy as np
+from functools import lru_cache as cache  # Python 3.9
+from helpers.utils import timer
 
 
 def gcd(a: int, b: int, do_print: bool = False, return_s_t: bool = False) -> int:
@@ -28,6 +31,19 @@ def totient(n: int) -> int:
     return sum(gcd(n, i) == 1 for i in range(1, n+1))
 
 
+# @cache
+def primes_ip_to(n):
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = ones(n//3 + (n % 6 == 2), dtype=np.bool)
+    sieve[0] = False
+    for i in range(int(n**0.5)//3+1):
+        if sieve[i]:
+            k = 3*i+1 | 1
+            sieve[((k*k)//3)::2*k] = False
+            sieve[(k*k+4*k-2*k*(i & 1))//3::2*k] = False
+    return r_[2, 3, ((3*nonzero(sieve)[0]+1) | 1)]
+
+
 if __name__ == "__main__":
     assert gcd(10, 12) == 2
     assert gcd(8, 13) == 1
@@ -39,3 +55,8 @@ if __name__ == "__main__":
     assert totient(301) == 252
     assert totient(1) == 1
     assert totient(0) == 0
+    print(primes_ip_to(11))
+
+    with timer():
+        for i in range(100):
+            totient(i)
