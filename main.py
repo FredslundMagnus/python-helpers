8effd0bcfe79fcf08ecde15bdc490e81e70400d1
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-02-08 22:02:16 +0100

    fix

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ class Defaults(Parameters):
     instances: int = 1
     GPU: bool = False
     time: int = 3600
-    database: Database = Database("dtu-server-test")
+    # database: Database = Database("dtu-server-test")
 
     b: float = 2.0
     a: int = 1
     d: str = "fd"
 
-    def run(self, b: float, d: str, a: int, database: Database) -> None:
-        database.set("doc1", {"a": a, "b": b})
-        print(database.get("doc1"))
-        print(b,d, self.time, color=Colors.green)
+    def run(self, b: float, d: str) -> None:
+        # database.set("doc1", {"a": a, "b": b})
+        # print(database.get("doc1"))
+        print(b,d, self.time, color=Colors.red)
 
 
 Defaults.start()
